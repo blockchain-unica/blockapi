53c4f5ad460defd564dd568e3d275383fbe44228
Stefano Lande <landeste@gmail.com>
2017-06-12 17:34:57 +0200

    BitcoinScript

--- BlockchainDLib/src/main/scala/tcs/blockchain/bitcoin/BitcoinScript.scala
+++ BlockchainDLib/src/main/scala/tcs/blockchain/bitcoin/BitcoinScript.scala
@@ -1,8 +1,12 @@
 package tcs.blockchain.bitcoin
 
+import org.bitcoinj.script.Script
+
+
+
 /**
   * Created by Livio on 12/06/2017.
   */
-class BitcoinScript {
+class BitcoinScript(bytes: Array[Byte]) extends Script(bytes){
 
 }
