1c40d9a127efd346d2de2461f1f8faf06b86e68e
LivioPompianu <pompianu.livio@gmail.com>
2017-07-14 15:38:36 +0200

    Documented BitcoinOutput file

--- src/main/scala/tcs/blockchain/bitcoin/BitcoinOutput.scala
+++ src/main/scala/tcs/blockchain/bitcoin/BitcoinOutput.scala
@@ -9,17 +9,40 @@ import org.bitcoinj.params.{MainNetParams, TestNet3Params}
 import scala.collection.mutable
 
 /**
-  * Created by Livio on 12/06/2017.
+  * Defines a transaction output of the Bitcoin blockchain.
+  *
+  * @param value Output value (in Satoshy).
+  * @param index Index of the output (w.r.t. the transaction containing this output).
+  * @param outScript Output script.
   */
-
 class BitcoinOutput(
                      val index: Int,
                      val value: Long,
                      val outScript: BitcoinScript) {
 
+
+  /**
+    * String representation of a BitcoinOutput.
+    *
+    * @return String representation of a BitcoinOutput.
+    */
   override def toString(): String =
     index + " " + value + " " + outScript
 
+
+  /**
+    * True if the output script uses the OP_RETURN operator.
+    *
+    * @return True when OP_RETURN is used.
+    */
+  def isOpreturn(): Boolean = outScript.isOpReturn
+
+
+  /**
+    * Returns the appended metadata if the output script uses the OP_RETURN operator.
+    *
+    * @return Either the metadata or null.
+    */
   def getMetadata(): String =
     if (!isOpreturn) null else {
       var v1: Integer = outScript.toString.indexOf("[");
@@ -30,8 +53,14 @@ class BitcoinOutput(
         return outScript.toString.substring(v1 + 1, v2)
     }
 
-  def isOpreturn(): Boolean = outScript.isOpReturn
 
+  /**
+    * Returns the address that received the funds of this transaction
+    * output, when the information is available.
+    *
+    * @param network Network settings.
+    * @return Either the recipient address or None.
+    */
   def getAddress(network: Network): Option[Address] = {
     try {
       if (outScript.isPayToScriptHash || outScript.isSentToAddress) {
@@ -46,7 +75,19 @@ class BitcoinOutput(
   }
 }
 
+
+/**
+  * Factories for [[tcs.blockchain.bitcoin.BitcoinOutput]] instances.
+  */
 object BitcoinOutput {
+
+  /**
+    * Factory for [[tcs.blockchain.bitcoin.BitcoinOutput]] instances.
+    * Creates a new output given its BitcoinJ representation.
+    *
+    * @param output BitcoinJ representation of the output.
+    * @return A new BitcoinOutput.
+    */
   def factory(output: TransactionOutput): BitcoinOutput = {
     new BitcoinOutput(output.getIndex,
       output.getValue.longValue(),
@@ -59,8 +100,18 @@ object BitcoinOutput {
   }
 
 
+  /**
+    * Factory for [[tcs.blockchain.bitcoin.BitcoinOutput]] instances.
+    * Creates a new output given its BitcoinJ representation.
+    *
+    * @param output BitcoinJ representation of the output.
+    * @param txHash Hash of the enclosing transaction.
+    * @param UTXOmap Unspent transaction outputs map.
+    * @return A new BitcoinOutput.
+    */
   def factory(output: TransactionOutput, txHash: Sha256Hash, UTXOmap: mutable.HashMap[(Sha256Hash, Long), Long]): BitcoinOutput = {
 
+    // Adds output value to the map, in order to retrieve it when evaluating input values.
     UTXOmap += ((txHash, output.getIndex.toLong) -> output.getValue.longValue())
 
     new BitcoinOutput(output.getIndex,
