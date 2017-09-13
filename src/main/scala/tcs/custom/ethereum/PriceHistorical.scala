e855f9b941954234f47fd94b62c021c583c42886
Ferruvich <ferruvich@gmail.com>
2017-09-13 22:57:02 +0200

    merging with ethereum-analytics

---
+++ src/main/scala/tcs/custom/ethereum/PriceHistorical.scala
@@ -0,0 +1,70 @@
+package tcs.custom.ethereum
+
+import java.io.{BufferedReader, InputStreamReader}
+import java.net.{HttpURLConnection, URL}
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.codahale.jerkson.Json.parse
+import com.codesnippets4all.json.parsers.JsonParserFactory
+import tcs.pojos.{CoinMarketPrices, CoinMarketPricesRaw}
+
+import scala.io.Source
+
+/**
+  * Created by ferruvich on 16/08/2017.
+  */
+object PriceHistorical {
+
+  private var marketCap: List[MarketCap] = null
+
+  def getPriceHistorical(): CoinMarketPrices = {
+    val url = new URL("https://graphs.coinmarketcap.com/currencies/ethereum/")
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.setRequestMethod("GET")
+    val br = new BufferedReader(new InputStreamReader(connection.getInputStream))
+    val str = Stream.continually(br.readLine()).takeWhile(_ != null).mkString("\n")
+    val coinMarketRaw = parse[CoinMarketPricesRaw](str)
+
+    val format = new SimpleDateFormat("yyyy-MM-dd")
+
+    val coinMarket = CoinMarketPrices(coinMarketRaw.market_cap_by_available_supply.map(l => format.format(new Date(l.head.longValue())) -> l.last.intValue()).toMap[String, Int],
+      coinMarketRaw.price_btc.map(l => format.format(new Date(l.head.longValue())) -> l.last.doubleValue()).toMap[String, Double],
+      coinMarketRaw.price_usd.map(l => format.format(new Date(l.head.longValue())) -> l.last.doubleValue()).toMap[String, Double])
+    coinMarket
+  }
+
+  def getPriceHistorical(time: Long): String = {
+    val url = new URL("https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts=" + time + "&markets=Coinbase")
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.setRequestMethod("GET")
+    val br = new BufferedReader(new InputStreamReader(connection.getInputStream))
+    val str = Stream.continually(br.readLine()).takeWhile(_ != null).mkString("\n")
+
+    val factory = JsonParserFactory.getInstance
+    val parser = factory.newJsonParser
+    val map = parser.parseJson(str)
+    val usd = map.get("ETH").asInstanceOf[java.util.HashMap[String, String]]
+
+    usd.get("USD")
+  }
+
+  def getMarketCapList(): List[MarketCap] = {
+    if(this.marketCap == null){
+      prepareMarketCapList()
+    }
+    this.marketCap
+  }
+
+  private def prepareMarketCapList(): Unit = {
+    val bufferedSource = Source.fromFile("src/main/scala/tcs/custom/regression/export-MarketCap.csv")
+    this.marketCap = List[MarketCap]()
+    val lines = bufferedSource.getLines()
+    lines.foreach(line => {
+      val cols = line.split(",")
+      if(!cols(0).contains("Date")){
+        this.marketCap ::= new MarketCap(cols(0).replace("\"", ""), BigDecimal(cols(1).replace("\"", "")), BigDecimal(cols(2).replace("\"", "")), BigDecimal(cols(3).replace("\"", "")))
+      }
+    })
+  }
+}
---
+++ src/main/scala/tcs/examples/ethereum/TxWithRates.scala
@@ -0,0 +1,44 @@
+package tcs.examples.ethereum
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import tcs.blockchain.BlockchainLib
+import tcs.custom.ethereum.PriceHistorical
+import tcs.mongo.Collection
+import tcs.db.DatabaseSettings
+
+object TxWithRates {
+  def main(args: Array[String]): Unit = {
+    val blockchain = BlockchainLib.getEthereumBlockchain("http://localhost:8545").setStart(70000).setEnd(150000)
+    val mongo = new DatabaseSettings()("myDatabase")
+    val weiIntoEth = BigInt("1000000000000000000")
+    val txWithRates = new Collection("txWithRates", mongo)
+    val format = new SimpleDateFormat("yyyy-MM-dd")
+    val priceHistorical = PriceHistorical.getPriceHistorical()
+
+    blockchain.foreach(block => {
+      if(block.number % 1000 == 0){
+        println("Current block ->" + block.number)
+      }
+      val date = new Date(block.timeStamp.longValue()*1000)
+      val dateFormatted = format.format(date)
+      block.transactions.foreach(tx => {
+        val to = if(tx.to == null) "" else tx.to
+        val list = List(
+          ("txHash", tx.hash),
+          ("blockHeight", tx.blockNumber.toString()),
+          ("txIndex", tx.transactionIndex),
+          ("date", date),
+          ("from", tx.from),
+          ("to", to),
+          ("value", tx.value.doubleValue()/weiIntoEth.doubleValue()),
+          ("rate", if(block.timeStamp.longValue() < 1438905600) 0 else priceHistorical.price_usd(dateFormatted))
+        )
+        txWithRates.append(list)
+      })
+    })
+
+    txWithRates.close
+  }
+}
