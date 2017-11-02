7ee88a91ffa50529c46f31af4c91023514a406b5
Stefano Lande <landeste@gmail.com>
2017-11-02 09:16:01 +0100

    minor

--- src/main/scala/tcs/examples/bitcoin/mongo/TxWithRates.scala
+++ src/main/scala/tcs/examples/bitcoin/mongo/TxWithRates.scala
@@ -5,6 +5,7 @@ import tcs.blockchain.bitcoin.{BitcoinSettings, MainNet}
 import tcs.custom.bitcoin.Exchange
 import tcs.db.DatabaseSettings
 import tcs.mongo.Collection
+import tcs.utils.DateConverter
 
 /**
   * Created by Livio on 13/06/2017.
@@ -18,7 +19,11 @@ object TxWithRates {
 
     val txWithRates = new Collection("txWithRates", mongo)
 
-    blockchain.foreach(block => {
+    blockchain.end(473100).foreach(block => {
+
+      if (block.height % 10000 == 0) println(DateConverter.formatTimestamp(System.currentTimeMillis()) + " - Block: " + block.height)
+
+
       block.bitcoinTxs.foreach(tx => {
         txWithRates.append(List(
           ("txHash", tx.hash),
