964eb481a3284624ccb21699d292361907cc6127
Ferruvich <ferruvich@gmail.com>
2018-01-15 17:10:21 +0100

    Done a little refactoring. Added ICOBenchAPI.getAllICOs method.

---
+++ src/main/scala/tcs/custom/ethereum/ICOBenchAPIs/BenchResult.scala
@@ -0,0 +1,8 @@
+package tcs.custom.ethereum.ICOBenchAPIs
+
+case class BenchResult(
+                      icos: Int,
+                      pages: Int,
+                      currentPage: Int,
+                      results: Array[ICOResult]
+                      )
