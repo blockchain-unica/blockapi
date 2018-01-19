e6a6a7bee1b6b5cfe8ad41dd1ca2a550fbe1346a
Ferruvich <ferruvich@gmail.com>
2018-01-19 18:21:12 +0100

    Changed example package, divided ethereum folder in ethereum/mongo and ethereum/sql

---
+++ src/main/scala/tcs/examples/ethereum/mongo/TxWithFees.scala
@@ -0,0 +1,46 @@
+package tcs.examples.ethereum.mongo
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import tcs.blockchain.BlockchainLib
+import tcs.custom.ethereum.PriceHistorical
+import tcs.db.DatabaseSettings
+import tcs.mongo.Collection
+
+object TxWithFees {
+  def main(args: Array[String]): Unit = {
+    val blockchain = BlockchainLib.getEthereumBlockchain("http://localhost:8545")
+      .setStart(70000).setEnd(150000)
+    val mongo = new DatabaseSettings("myDatabase")
+    val weiIntoEth = BigInt("1000000000000000000")
+    val txWithFees = new Collection("txWithFees", mongo)
+    val format = new SimpleDateFormat("yyyy-MM-dd")
+    val priceHistorical = PriceHistorical.getPriceHistorical()
+
+    blockchain.foreach(block => {
+      if(block.number % 1000 == 0){
+        println("Current block ->" + block.number)
+      }
+      val date = new Date(block.timeStamp.longValue()*1000)
+      val dateFormatted = format.format(date)
+      block.transactions.foreach(tx => {
+        val creates = if(tx.creates == null) "" else tx.creates
+        val to = if(tx.to == null) "" else tx.to
+        val list = List(
+          ("blockHash", block.hash),
+          ("txHash", tx.hash),
+          ("date", date),
+          ("value", tx.value.doubleValue()/weiIntoEth.doubleValue()),
+          ("creates", creates),
+          ("gas", tx.gas),
+          ("fee", (tx.gas * tx.gasPrice)/weiIntoEth),
+          ("rate", if(block.timeStamp.longValue() < 1438905600) 0 else priceHistorical.price_usd(dateFormatted))
+        )
+        txWithFees.append(list)
+      })
+    })
+
+    txWithFees.close
+  }
+}
---
+++ src/main/scala/tcs/examples/ethereum/mongo/TxWithRates.scala
@@ -0,0 +1,47 @@
+package tcs.examples.ethereum.mongo
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import tcs.blockchain.BlockchainLib
+import tcs.custom.ethereum.PriceHistorical
+import tcs.db.DatabaseSettings
+import tcs.mongo.Collection
+
+object TxWithRates {
+  def main(args: Array[String]): Unit = {
+    val blockchain = BlockchainLib.getEthereumBlockchain("http://localhost:8545")
+      .setStart(70000).setEnd(150000)
+    val mongo = new DatabaseSettings("myDatabase")
+    val weiIntoEth = BigInt("1000000000000000000")
+    val txWithRates = new Collection("txWithRates", mongo)
+    val format = new SimpleDateFormat("yyyy-MM-dd")
+    val priceHistorical = PriceHistorical.getPriceHistorical()
+
+    blockchain.foreach(block => {
+      if(block.number % 1000 == 0){
+        println("Current block ->" + block.number)
+      }
+      val date = new Date(block.timeStamp.longValue()*1000)
+      val dateFormatted = format.format(date)
+      block.transactions.foreach(tx => {
+        val creates = if(tx.creates == null) "" else tx.creates
+        val to = if(tx.to == null) "" else tx.to
+        val list = List(
+          ("txHash", tx.hash),
+          ("blockHeight", tx.blockNumber.toString()),
+          ("txIndex", tx.transactionIndex),
+          ("date", date),
+          ("from", tx.from),
+          ("to", to),
+          ("value", tx.value.doubleValue()/weiIntoEth.doubleValue()),
+          ("creates", creates),
+          ("rate", if(block.timeStamp.longValue() < 1438905600) 0 else priceHistorical.price_usd(dateFormatted))
+        )
+        txWithRates.append(list)
+      })
+    })
+
+    txWithRates.close
+  }
+}
