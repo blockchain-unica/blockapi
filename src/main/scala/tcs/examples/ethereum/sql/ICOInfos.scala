ab04ed6ec933ac4ba81e848b9090b48cbed05b4c
Ferruvich <ferruvich@gmail.com>
2018-01-22 18:36:05 +0100

    Testing

--- src/main/scala/tcs/examples/ethereum/sql/ICOInfos.scala
+++ src/main/scala/tcs/examples/ethereum/sql/ICOInfos.scala
@@ -1,21 +1,49 @@
 package tcs.examples.ethereum.sql
 
+import scalikejdbc._
 import tcs.blockchain.BlockchainLib
-import tcs.custom.ethereum.ICO
-import tcs.db.{DatabaseSettings, MySQL}
+import tcs.db.mysql.Table
+import tcs.db.{DatabaseSettings, PostgreSQL}
 
 
 object ICOInfos {
   def main(args: Array[String]): Unit = {
-    val ico = new ICO("0xe41d2489571d322189246dafa5ebde1f4699f498")
-    println(ico.getName)
-    println(ico.getSymbol)
-    println(ico.getUSDPrice)
-    println(ico.getETHPrice)
-    println(ico.getBTCPrice)
-    println(ico.getMarketCap)
-    //val blockchain = BlockchainLib.getEthereumBlockchain("http://localhost:8545")
-    //  .setStart(0).setEnd(1500000)
-    //val pg = new DatabaseSettings("ethereum", MySQL, "postgres")
+    val blockchain = BlockchainLib.getEthereumBlockchain("http://localhost:8545")
+      .setStart(0).setEnd(1370000)
+    val pg = new DatabaseSettings("ethereum", PostgreSQL, "postgres")
+
+    val icoTable = new Table(
+      sql"""
+         CREATE TABLE IF NOT EXISTS ico(
+          icoId INTEGER NOT NULL PRIMARY KEY,
+          tokenName CHARACTER VARYING(25),
+          tokenSymbol CHARACTER VARYING(10),
+          contractId INTEGER NOT NULL,
+          marketCap NUMERIC(20,2),
+          totalSupply NUMERIC(20,2),
+          blockchain CHARACTER VARYING(15),
+          priceUSD NUMERIC(8,8),
+          priceETH NUMERIC(8,8),
+          priceBTC NUMERIC(8,8),
+          hypeScore NUMERIC(3,2),
+          riskScore NUMERIC(3,2),
+          investmentRating CHARACTER VARYING(10)
+         ) """,
+      sql"""
+          INSERT INTO ico(tokenName, tokenSymbol, contractId, marketCap,
+          totalSupply, blockchain, priceUSD, priceETH, priceBTC,
+          hypeScore, riskScore, investmentRating)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+         """,
+      pg)
+
+    val txTable = new Table(
+      sql"""
+
+         """,
+      sql"""
+         """,
+      pg
+    )
   }
 }
