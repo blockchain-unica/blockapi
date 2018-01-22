a0f0cb7d3c29b868d80204fe9157a837d88fe131
ferruvich <ferruvich@gmail.com>
2018-01-23 00:13:51 +0100

    Changing example

--- src/main/scala/tcs/examples/ethereum/sql/ICOInfos.scala
+++ src/main/scala/tcs/examples/ethereum/sql/ICOInfos.scala
@@ -1,27 +1,30 @@
 package tcs.examples.ethereum.sql
 
+import java.util.Date
+
 import scalikejdbc._
 import tcs.blockchain.BlockchainLib
 import tcs.custom.ethereum.ICO
 import tcs.db.sql.Table
 import tcs.db.{DatabaseSettings, PostgreSQL}
+import tcs.utils.DateConverter.convertDate
 
 
 object ICOInfos {
   def main(args: Array[String]): Unit = {
     val blockchain = BlockchainLib.getEthereumBlockchain("https://mainnet.infura.io/OCPoiiZvFpsPKZcOMGaG")
-      .setStart(0).setEnd(1370000)
+      .setStart(1000000).setEnd(4000000)
     val pg = new DatabaseSettings("ethereum", PostgreSQL, "postgres")
 
     val blockTable = new Table(
       sql"""
           CREATE TABLE IF NOT EXISTS block(
-            hash CHARACTER VARYING(50) NOT NULL PRIMARY KEY,
+            hash CHARACTER VARYING(100) NOT NULL PRIMARY KEY,
             number INTEGER,
-            parentHash CHARACTER VARYING(50) REFERENCES block(hash),
+            parentHash CHARACTER VARYING(100),
             timestamp TIMESTAMP,
-            author CHARACTER VARYING(50),
-            miner CHARACTER VARYING(50)
+            author CHARACTER VARYING(100),
+            miner CHARACTER VARYING(100)
           )
          """,
       sql"""
@@ -29,40 +32,39 @@ object ICOInfos {
           author, miner)
           VALUES (?, ?, ?, ?, ?, ?)
          """,
-      pg
+      pg, 1
     )
 
     val txTable = new Table(
       sql"""
           CREATE TABLE IF NOT EXISTS transaction(
-            hash CHARACTER VARYING(50) NOT NULL PRIMARY KEY,
+            hash CHARACTER VARYING(100) NOT NULL PRIMARY KEY,
             nonce NUMERIC(30,2),
             transactionIndex INTEGER,
-            txFrom CHARACTER VARYING(50),
-            txTo CHARACTER VARYING(50),
-            txInput CHARACTER VARYING(50),
+            txFrom CHARACTER VARYING(100),
+            txTo CHARACTER VARYING(100),
             txValue NUMERIC(30,2),
             gas NUMERIC(30,2),
             gasPrice NUMERIC(30,2),
-            blockHash CHARACTER VARYING(50) REFERENCES block(hash)
+            blockHash CHARACTER VARYING(100) REFERENCES block(hash)
           )
          """,
       sql"""
             INSERT INTO transaction(hash, nonce, transactionIndex,
-            txFrom, txTo, txInput, txValue, gas, gasPrice)
+            txFrom, txTo, txValue, gas, gasPrice, blockHash)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
          """,
-      pg
+      pg, 100
     )
 
     val internalTxTable = new Table(
       sql"""
           CREATE TABLE IF NOT EXISTS internal_transaction(
             id INTEGER NOT NULL PRIMARY KEY,
-            parentTxHash CHARACTER VARYING(50) REFERENCES transaction(hash),
-            txType CHARACTER VARYING(50),
-            itxFrom CHARACTER VARYING(50),
-            itxTo CHARACTER VARYING(50),
+            parentTxHash CHARACTER VARYING(100) REFERENCES transaction(hash),
+            txType CHARACTER VARYING(100),
+            itxFrom CHARACTER VARYING(100),
+            itxTo CHARACTER VARYING(100),
             itxValue NUMERIC(30,2)
           )
          """,
@@ -71,7 +73,7 @@ object ICOInfos {
             itxFrom, itxTo, itxValue)
             VALUES (?, ?, ?, ?, ?)
          """,
-      pg
+      pg, 100
     )
 
     val icoTable = new Table(
@@ -80,7 +82,7 @@ object ICOInfos {
           icoId INTEGER NOT NULL PRIMARY KEY,
           tokenName CHARACTER VARYING(25),
           tokenSymbol CHARACTER VARYING(10),
-          contractAddress CHARACTER VARYING(50),
+          contractAddress CHARACTER VARYING(100),
           marketCap NUMERIC(20,2),
           totalSupply NUMERIC(20,2),
           blockchain CHARACTER VARYING(15),
@@ -90,7 +92,7 @@ object ICOInfos {
           hypeScore NUMERIC(3,2),
           riskScore NUMERIC(3,2),
           investmentRating CHARACTER VARYING(10),
-          txCreatorHash CHARACTER VARYING(50) REFERENCES transaction(hash)
+          txCreatorHash CHARACTER VARYING(100) REFERENCES transaction(hash)
          ) """,
       sql"""
           INSERT INTO ico(tokenName, tokenSymbol, contractId, marketCap,
@@ -98,22 +100,25 @@ object ICOInfos {
           hypeScore, riskScore, investmentRating)
           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
          """,
-      pg)
+      pg, 100
+    )
 
     blockchain.foreach(block => {
-      println(block.number)
+      if(block.number % 100 == 0){
+        println(block.number)
+      }
       blockTable.insert(Seq(
         block.hash, block.number, block.parentHash,
-        block.timeStamp, block.author, block.miner
+        new Date(block.timeStamp.longValue()*1000), block.author, block.miner
       ))
       block.transactions.foreach(tx => {
         txTable.insert(Seq(
           tx.hash, tx.nonce, tx.transactionIndex, tx.from,
-          tx.to, tx.input, tx.value, tx.gas, tx.gasPrice,
+          tx.to, tx.value, tx.gas, tx.gasPrice,
           tx.blockHash
         ))
-        if(!tx.creates.isEmpty){
-          try{
+        if (tx.creates != null) {
+          try {
             val ico = new ICO(tx.creates)
             icoTable.insert(Seq(
               ico.getName, ico.getSymbol, ico.getContractAddress,
