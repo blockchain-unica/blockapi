bfcb141da615153ad9c9a98dac1b8efaa3feed20
Stefano Lande <landeste@gmail.com>
2018-02-16 15:31:41 +0100

    added bitcoind host to settings

--- src/main/scala/tcs/blockchain/bitcoin/BitcoinSettings.scala
+++ src/main/scala/tcs/blockchain/bitcoin/BitcoinSettings.scala
@@ -3,18 +3,37 @@ package tcs.blockchain.bitcoin
 /**
   * Framework settings for the Bitcoin blockchain.
   *
-  * @param rpcUser Bitcoin Core user.
-  * @param rpcPassword Bitcoin Core password.
-  * @param rpcPort Bitcoin Core port.
-  * @param network Either Bitcoin Main network or Bitcoin Test network.
+  * @param rpcUser             Bitcoin Core user.
+  * @param rpcPassword         Bitcoin Core password.
+  * @param rpcPort             Bitcoin Core port.
+  * @param rpcHost             Bitcoin Core address.
+  * @param network             Either Bitcoin Main network or Bitcoin Test network.
   * @param retrieveInputValues True for performing a "deep scan" of the blockchain and retrieve input values.
   */
 class BitcoinSettings(
                        val rpcUser: String,
                        val rpcPassword: String,
                        val rpcPort: String,
+                       val rpcHost: String,
                        val network: Network,
                        val retrieveInputValues: Boolean = false) {
+
+  /**
+    * Framework settings for the Bitcoin blockchain.
+    * Sets the Bitcoin Core host to localhost
+    *
+    * @param rpcUser             Bitcoin Core user.
+    * @param rpcPassword         Bitcoin Core password.
+    * @param rpcPort             Bitcoin Core port.
+    * @param network             Either Bitcoin Main network or Bitcoin Test network.
+    * @param retrieveInputValues True for performing a "deep scan" of the blockchain and retrieve input values.
+    */
+
+  def this(rpcUser: String,
+           rpcPassword: String,
+           rpcPort: String,
+           network: Network,
+           retrieveInputValues: Boolean = false) = this(rpcUser, rpcPassword, rpcPort, "localhost", network, retrieveInputValues)
 
 }
 
