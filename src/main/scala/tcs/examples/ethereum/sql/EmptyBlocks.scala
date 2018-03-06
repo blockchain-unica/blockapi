056d354e7e2bf7b8c729b69eee065cfbec377683
Omar Desogus <omardesogus9@gmail.com>
2018-03-06 13:38:20 +0100

    code refactoring

--- src/main/scala/tcs/examples/ethereum/sql/EmptyBlocks.scala
+++ src/main/scala/tcs/examples/ethereum/sql/EmptyBlocks.scala
@@ -10,11 +10,8 @@ import tcs.db.{DatabaseSettings, MySQL}
 
 object EmptyBlocks {
   def main(args: Array[String]): Unit = {
-    //val blockchain = BlockchainLib.getEthereumBlockchain("http://localhost:8545")
     val blockchain = BlockchainLib.getEthereumBlockchain("https://mainnet.infura.io/lGhdnAJw7n56K0xXGP3i:8545")
-
-      .setStart(3224233)
-    val pg = new DatabaseSettings("ethereum", MySQL, "root", "10d1108c")
+    val pg = new DatabaseSettings("ethereum", MySQL, "root", "toor")
 
     val blockTable = new Table(
       sql"""
@@ -32,13 +29,12 @@ object EmptyBlocks {
 
 
     blockchain.foreach(block => {
+      if (block.number % 100 == 0) {
+        println(block.number)
+      }
 
       if (block.transactions.isEmpty) {
         blockTable.insert(Seq(block.hash, new Date(block.timeStamp.longValue() * 1000), block.miner))
-      }
-
-      if (block.number % 100 == 0) {
-        println(block.number)
       }
     })
 
