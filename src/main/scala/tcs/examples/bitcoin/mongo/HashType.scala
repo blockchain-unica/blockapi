371508d5bb688a91bf9e72711eed3e8092b2e5ad
H1cham <hichamlafhouli@gmail.com>
2018-03-14 20:24:47 +0100

    create fix

--- src/main/scala/tcs/examples/bitcoin/mongo/HashType.scala
+++ src/main/scala/tcs/examples/bitcoin/mongo/HashType.scala
@@ -10,34 +10,42 @@ import tcs.utils.{HashTypeUtils}
 
 /**
   * Created by
-  *         Chelo Fabrizio
-  *         Lafhouli Hicham
-  *         Meloni Antonello
+  * Chelo Fabrizio
+  * Lafhouli Hicham
+  * Meloni Antonello
   */
 
-object  HashType{
+object HashType {
   def main(args: Array[String]): Unit = {
 
     val blockchain = BlockchainLib.getBitcoinBlockchain(new BitcoinSettings("user", "password", "8332", MainNet))
     val mongo = new DatabaseSettings("myDatabase")
+    val hashType = new Collection("HashType1", mongo)
 
-    val hashType = new Collection("HashType", mongo)
     blockchain.foreach(block => {
+
       block.bitcoinTxs.foreach(tx => {
+
         var inputIndex: Integer = 0
+
         tx.inputs.foreach(in => {
 
+
           if (in.redeemedOutIndex >= 0) {
+            
             val signatures = HashTypeUtils.parsing(in.script)
+            
             if (!(signatures == null)) {
 
               signatures.forEach { s => {
+
                 hashType.append(List(
                   ("txHash", tx.hash),
-                  ("index", inputIndex) ,
+                  ("index", inputIndex),
                   ("date", block.date),
                   ("hashType", in.getHashType(s))
                 ))
+
               }
               }
             }
@@ -50,9 +58,6 @@ object  HashType{
       )
     }
     )
-
     hashType.close
   }
 }
-
-
