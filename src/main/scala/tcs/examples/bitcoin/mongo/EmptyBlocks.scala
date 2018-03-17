e1ce50538c626af9caa0caf140c41f729f118742
Jeeiii <GiacomooJeii@gmail.com>
2018-03-17 01:08:17 +0100

    Fix
    - Removed emptyBlocks collection
    - Added boolean for checking if a block is empty
    - Queries updates

--- src/main/scala/tcs/examples/bitcoin/mongo/EmptyBlocks.scala
+++ src/main/scala/tcs/examples/bitcoin/mongo/EmptyBlocks.scala
@@ -14,26 +14,27 @@ object EmptyBlocks {
     val blockchain = BlockchainLib.getBitcoinBlockchain(new BitcoinSettings("user", "password", "8332", MainNet))
     val mongo = new DatabaseSettings("blocksDB")
 
-    val blocks = new Collection("blocks", mongo) // Collection for all blocks (used for analysis)
-    val emptyBlocks = new Collection("emptyblocks", mongo) // Collection for empty blocks
+    val blocks = new Collection("blocks", mongo) // Collection for all blocks
 
     // Iterating each block
     blockchain.foreach(block => {
       // checking if the block it's "empty" (i.e. only the coinbase tx)
       if (block.bitcoinTxs.lengthCompare(1) == 0)
-        // adding the empty block into the collection emptyblocks
-        emptyBlocks.append(List(
-          ("blockHash", block.hash),
-          ("date", block.date)
-        ))
-        // Adding the block into the collection blocks
+      // adding the empty block into the collection emptyblocks
         blocks.append(List(
           ("blockHash", block.hash),
-          ("date", block.date)
+          ("date", block.date),
+          ("isEmpty", true)
+        ))
+      else
+      // Adding the block into the collection blocks
+        blocks.append(List(
+          ("blockHash", block.hash),
+          ("date", block.date),
+          ("isEmpty", false)
         ))
     })
 
     blocks.close
-    emptyBlocks.close
   }
 }
