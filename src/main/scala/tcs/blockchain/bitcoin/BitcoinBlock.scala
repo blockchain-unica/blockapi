2b4a8461f8e83cfbe58a5f2298ee9a0d053eb964
LivioPompianu <pompianu.livio@gmail.com>
2018-03-19 15:28:50 +0100

    Improved Bitcoin pools

--- src/main/scala/tcs/blockchain/bitcoin/BitcoinBlock.scala
+++ src/main/scala/tcs/blockchain/bitcoin/BitcoinBlock.scala
@@ -1,14 +1,12 @@
 package tcs.blockchain.bitcoin
 
 import java.util.Date
-import javax.xml.bind.DatatypeConverter
 
 import org.bitcoinj.core.{Block, Sha256Hash}
 import tcs.blockchain.{Block => TCSBlock}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
-import scala.util.matching.Regex
 
 /**
   * Defines a block of the Bitcoin blockchain.
@@ -55,38 +53,37 @@ class BitcoinBlock(
   }
 
   private def getPoolByHexCode(hex: String): String = {
-    var returned: String = "Unknown";
 
     // Known pool codes listed on blockchain.info
     // See https://github.com/blockchain/Blockchain-Known-Pools/blob/master/pools.json for more
-    if(hex.contains("416e74506f6f6c3")) returned = "AntPool"
-    if(hex.contains("42697446757279")) returned = "BitFury"
-    if(hex.contains("736c757368")) returned = "SlushPool"
-    if(hex.contains("42544343")) returned = "BTCCPool"
-    if(hex.contains("4254432e434f4d")) returned = "BTC.COM"
-    if(hex.contains("566961425443")) returned = "ViaBTC"
-    if(hex.contains("4254432e544f502")) returned = "BTC.TOP"
-    if(hex.contains("426974436c7562204e6574776f726b")) returned = "Bitclub Network"
-    if(hex.contains("67626d696e657273")) returned = "GBMiners"
-    if(hex.contains("42697466757279")) returned = "Bitfury"
-    if(hex.contains("4269744d696e746572")) returned = "BitMinter"
-    if(hex.contains("4b616e6f")) returned = "KanoPool"
-    if(hex.contains("426974636f696e2d5275737369612e7275")) returned = "BitcoinRussia"
-    if(hex.contains("426974636f696e2d496e646961")) returned = "BitcoinIndia"
-    if(hex.contains("425720506f6f6c")) returned = "BW.COM"
-    if(hex.contains("3538636f696e2e636f6d")) returned = "58coin"
-    if(hex.contains("706f6f6c2e626974636f696e2e636f6d")) returned = "Bitcoin.com"
-    if(hex.contains("436f6e6e656374425443202d20486f6d6520666f72204d696e657273")) returned = "ConnectedBTC"
+    if(hex.contains("416e74506f6f6c3")) return "AntPool"
+    if(hex.contains("42697446757279")) return "BitFury"
+    if(hex.contains("736c757368")) return "SlushPool"
+    if(hex.contains("42544343")) return "BTCCPool"
+    if(hex.contains("4254432e434f4d")) return "BTC.COM"
+    if(hex.contains("566961425443")) return "ViaBTC"
+    if(hex.contains("4254432e544f502")) return "BTC.TOP"
+    if(hex.contains("426974436c7562204e6574776f726b")) return "Bitclub Network"
+    if(hex.contains("67626d696e657273")) return "GBMiners"
+    if(hex.contains("42697466757279")) return "Bitfury"
+    if(hex.contains("4269744d696e746572")) return "BitMinter"
+    if(hex.contains("4b616e6f")) return "KanoPool"
+    if(hex.contains("426974636f696e2d5275737369612e7275")) return "BitcoinRussia"
+    if(hex.contains("426974636f696e2d496e646961")) return "BitcoinIndia"
+    if(hex.contains("425720506f6f6c")) return "BW.COM"
+    if(hex.contains("3538636f696e2e636f6d")) return "58coin"
+    if(hex.contains("706f6f6c2e626974636f696e2e636f6d")) return "Bitcoin.com"
+    if(hex.contains("436f6e6e656374425443202d20486f6d6520666f72204d696e657273")) return "ConnectedBTC"
 
     // F2Pool does not have a unique identifier
-    if(hex.contains("777868")) returned = "F2Pool"
-    if(hex.contains("66326261636b7570")) returned = "F2Pool"
-    if(hex.contains("68663235")) returned = "F2Pool"
-    if(hex.contains("73796a756e303031")) returned = "F2Pool"
-    if(hex.contains("716c7339")) returned = "F2Pool"
-    if(hex.contains("687578696e6767616f7a68616f")) returned = "F2Pool"
+    if(hex.contains("777868")) return "F2Pool"
+    if(hex.contains("66326261636b7570")) return "F2Pool"
+    if(hex.contains("68663235")) return "F2Pool"
+    if(hex.contains("73796a756e303031")) return "F2Pool"
+    if(hex.contains("716c7339")) return "F2Pool"
+    if(hex.contains("687578696e6767616f7a68616f")) return "F2Pool"
 
-    return returned
+    return "Unknown"
   }
 }
 
