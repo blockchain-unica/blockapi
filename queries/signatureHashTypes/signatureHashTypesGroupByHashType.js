8ba8e2ed77b381eea635a6484d7a727a3d9bbecb
chelo47951 <chelo.fabrizio@tiscali.it>
2018-03-23 18:34:05 +0100

    Project SignatureHashTypes: added query for mongoDB to get the hashTypes grouped by hashType

---
+++ queries/signatureHashTypes/signatureHashTypesGroupByHashType.js
@@ -0,0 +1,7 @@
+db.signatureHashTypes.aggregate([
+{ 
+    $group: {
+    _id: "$hashType",  // group by hashType; we get the total count for each hash type
+    count: { $sum: 1}
+    }    
+}])
