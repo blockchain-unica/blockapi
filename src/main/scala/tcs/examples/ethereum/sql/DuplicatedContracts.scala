22f75bd6e6fc74730accd999b2c11e4049a7f361
fabseulo <fabiocarta90@hotmail.it>
2018-04-06 21:48:23 +0200

    Modifica MongoDB

--- src/main/scala/tcs/examples/ethereum/sql/DuplicatedContracts.scala
+++ src/main/scala/tcs/examples/ethereum/sql/DuplicatedContracts.scala
@@ -1,19 +1,22 @@
 package tcs.examples.ethereum.sql
 
-import scalikejdbc._
+//import scalikejdbc._
 import tcs.db.{DatabaseSettings, MySQL}
 import tcs.blockchain.BlockchainLib
 import tcs.blockchain.ethereum.EthereumSettings
-import tcs.db.sql.Table
+//import tcs.db.sql.Table
+import tcs.mongo.Collection
 
 object DuplicatedContracts {
 
   def main(args: Array[String]): Unit = {
 
-    val blockchain = BlockchainLib.getEthereumBlockchain(new EthereumSettings("http://localhost:8545", true))
-    val pg = new DatabaseSettings("ethereum", MySQL, "root", "toor")
+    val blockchain = BlockchainLib.getEthereumBlockchain(new EthereumSettings("https://mainnet.infura.io/lGhdnAJw7n56K0xXGP3i:8545", true))
+    val mongo = new DatabaseSettings("ethereum")
+    //val pg = new DatabaseSettings("ethereum", MySQL, "root", "toor")
+    val contracts = new Collection("contracts", mongo)
 
-    val contractTable = new Table(
+    /*val contractTable = new Table(
       sql"""
           CREATE TABLE IF NOT EXISTS contract(
             address CHARACTER VARYING(100) NOT NULL PRIMARY KEY,
@@ -26,7 +29,7 @@ object DuplicatedContracts {
           INSERT INTO contract(address, source_code, date, name) VALUES (?, ?, ?, ?)
          """,
       pg, 1
-    )
+    )*/
 
     blockchain.start(49400).foreach(block => {
 
@@ -36,15 +39,22 @@ object DuplicatedContracts {
 
       block.txs.foreach(tx => {
 
-        if (tx.hasContract && tx.contract.sourceCode.length>0) {
-          contractTable.insert(Seq(tx.contract.address, tx.contract.sourceCode, block.date, tx.contract.name));
+        println("Block: " + block.height + " Transaction: " + tx.hash + " Address created: " + tx.addressCreated)
 
+        if (tx.hasContract) {
+          val list = List(
+            ("contractAddress", tx.contract.address),
+            ("contractName", tx.contract.name),
+            ("date", block.date),
+            ("sourceCode", tx.contract.sourceCode)
+          )
+          contracts.append(list)
         }
 
       })
     })
 
-    contractTable.close
+    contracts.close
   }
 
 }
