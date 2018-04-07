d740ef094bc12a90effd11d01b8c6b187ef15cc5
mallociFrancesca <fmalloci@gmail.com>
2018-04-07 15:10:48 +0200

    created the html parsing to recover duplicated contracts

--- src/main/scala/tcs/examples/ethereum/sql/DuplicatedContracts.scala
+++ src/main/scala/tcs/examples/ethereum/sql/DuplicatedContracts.scala
@@ -1,22 +1,28 @@
 package tcs.examples.ethereum.sql
 
-//import scalikejdbc._
+import org.web3j.protocol.core.methods.response.EthBlock.TransactionObject
+import scalikejdbc._
 import tcs.db.{DatabaseSettings, MySQL}
 import tcs.blockchain.BlockchainLib
-import tcs.blockchain.ethereum.EthereumSettings
-//import tcs.db.sql.Table
-import tcs.mongo.Collection
+import tcs.blockchain.ethereum.{EthereumContract, EthereumSettings}
+import tcs.db.sql.Table
+import tcs.utils.HttpRequester
 
 object DuplicatedContracts {
-
   def main(args: Array[String]): Unit = {
 
-    val blockchain = BlockchainLib.getEthereumBlockchain(new EthereumSettings("https://mainnet.infura.io/lGhdnAJw7n56K0xXGP3i:8545", true))
-    val mongo = new DatabaseSettings("ethereum")
-    //val pg = new DatabaseSettings("ethereum", MySQL, "root", "toor")
-    val contracts = new Collection("contracts", mongo)
+    val blockchain = BlockchainLib.getEthereumBlockchain(new EthereumSettings("http://localhost:8545", true))//connection
+    val mongo = new DatabaseSettings("ethereum")//creates DB mongoDB
+    val contracts = new Collection("contracts", mongo) //creates the collection
 
-    /*val contractTable = new Table(
+    var number = "" //number of contracts duplicated
+    var addressContract ="" //temporaney address of contract duplicated
+
+    /*
+      SQL SETTINGS
+
+      val pg = new DatabaseSettings("ethereum", MySQL, "root", "toor")
+      val contractTable = new Table(
       sql"""
           CREATE TABLE IF NOT EXISTS contract(
             address CHARACTER VARYING(100) NOT NULL PRIMARY KEY,
@@ -41,20 +47,121 @@ object DuplicatedContracts {
 
         println("Block: " + block.height + " Transaction: " + tx.hash + " Address created: " + tx.addressCreated)
 
-        if (tx.hasContract) {
-          val list = List(
+        if (tx.hasContract)
+        {
+
+          try {
+
+                val content = HttpRequester.get("http://etherscan.io/find-similiar-contracts?a=" + tx.contract.address)
+                //println(content)
+                if(!content.contains(" Found a total of <b>0</b> EXACT match(es)<br><br>"))
+                {
+                  println("il contratto ha dei duplicati")
+
+                  // recupero il numero esatto di quante volte è stato duplicato
+                  val strForNumDuplicated = " Found a total of <b>"
+                  number = content.substring(content.indexOf(strForNumDuplicated)+strForNumDuplicated.length)
+                  number = number.substring(0, number.indexOf("<"))
+                  println(number)
+
+
+                  //indirizzi dei contratti che hanno lo stesso codice sorgente
+
+                  //memorizzo la tabella
+                  var subContent = content.substring(content.indexOf("</tr>\n<tr><td>Exact [100]</td><td>"),content.indexOf("</table>"))
+                  //println(subContent)
+                  val strForAddress ="</span></td><td><a href='/address/"
+
+                  do
+                  {
+                    subContent = subContent.substring(subContent.indexOf(strForAddress) + strForAddress.length)
+                    addressContract = subContent.substring(0, 42)
+                    println("###      indirizzo contratto      ###")
+                    println(addressContract)
+
+                  }while(subContent.contains(strForAddress))
+
+                }
+
+          } catch {
+                case ioe: java.io.IOException => {ioe.printStackTrace(); return tx.contract.address}
+                case ste: java.net.SocketTimeoutException => {ste.printStackTrace(); return tx.contract.address}
+                case e: Exception => {e.printStackTrace(); return tx.contract.address}
+          }
+
+
+        }//ifHasContrac
+      }) //foreach block
+    })//foreach blockChain
+  }//main
+}//object
+
+    /*  MONGO SETTINGS
+
+        val list = List(
             ("contractAddress", tx.contract.address),
             ("contractName", tx.contract.name),
             ("date", block.date),
             ("sourceCode", tx.contract.sourceCode)
-          )
-          contracts.append(list)
+                            )
+          //contracts.append(list)  //saves table into MongDB
         }
 
-      })
-    })
+        })
+        }
 
-    contracts.close
-  }
+    //contracts.close
+    */
 
-}
+    /*
+
+    SCRIPT PARSING DI ESEMPIO
+
+    val addressExample = "0x82EDa86610356B90e92ed23f2a5c3AA9670681A3"
+    var number = ""
+    var addressContract =""
+
+    try {
+
+      val content = HttpRequester.get("http://etherscan.io/find-similiar-contracts?a=" + addressExample)
+      //println(content)
+      if(!content.contains(" Found a total of <b>0</b> EXACT match(es)<br><br>"))
+        {
+          println("il contratto ha dei duplicati")
+
+          // recupero il numero esatto di quante volte è stato duplicato
+          val strForNumDuplicated = " Found a total of <b>"
+          number = content.substring(content.indexOf(strForNumDuplicated)+strForNumDuplicated.length)
+          number = number.substring(0, number.indexOf("<"))
+          println(number)
+
+
+          //indirizzi dei contratti che hanno lo stesso codice sorgente
+
+          //memorizzo la tabella
+          var subContent = content.substring(content.indexOf("</tr>\n<tr><td>Exact [100]</td><td>"),content.indexOf("</table>"))
+          //println(subContent)
+          val strForAddress ="</span></td><td><a href='/address/"
+
+          do
+          {
+            subContent = subContent.substring(subContent.indexOf(strForAddress) + strForAddress.length)
+            addressContract = subContent.substring(0, 42)
+            println("#########  indirizzo contratto ##########")
+            println(addressContract)
+
+          }while(subContent.contains(strForAddress))
+
+        }
+
+    } catch {
+      case ioe: java.io.IOException => {ioe.printStackTrace(); return addressExample}
+      case ste: java.net.SocketTimeoutException => {ste.printStackTrace(); return addressExample}
+      case e: Exception => {e.printStackTrace(); return addressExample}
+    }
+
+
+    */
+
+
+
