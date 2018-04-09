1e8b917c86fd3fbf5e552fc32e3629a7ae4c13db
Alessio Manai <alessio@manai.tk>
2018-04-09 14:15:58 +0200

    Implementazione isERC20Compliant versione 1

--- src/main/scala/tcs/blockchain/ethereum/EthereumContract.scala
+++ src/main/scala/tcs/blockchain/ethereum/EthereumContract.scala
@@ -4,16 +4,16 @@ import java.util.Date
 
 
 case class EthereumContract(
-                           val name : String,
-                           val address : String,
-                           val hashOriginatingTx : String,
+                             val name: String,
+                             val address: String,
+                             val hashOriginatingTx: String,
 
-                           val isVerified : Boolean,
-                           val verificationDate : Date,
+                             val isVerified: Boolean,
+                             val verificationDate: Date,
 
-                           val bytecode : String,
-                           val sourceCode : String
-  ){
+                             val bytecode: String,
+                             val sourceCode: String
+                           ) {
 
   val usesPermissions = isVerified && checkPermissions
 
@@ -24,8 +24,25 @@ case class EthereumContract(
     *
     * @return true if sourceCode contains permissions, false otherwise
     */
-  private def checkPermissions() : Boolean = {
+  private def checkPermissions(): Boolean = {
 
     return sourceCode.contains("modifier onlyOwner()")
   }
+
+  /**
+    * This method check if the contract is ERC20 compliant analyzing the bytecode
+    * source: https://ethereum.stackexchange.com/questions/38381/how-can-i-identify-that-transaction-is-erc20-token-creation-contract?answertab=oldest#tab-top
+    *
+    * @return true if the contract is ERC20 compliant, false otherwise
+    */
+
+  private def isERC20Compliant(): Boolean = {
+    return bytecode.contains("18160ddd") &&
+      bytecode.contains("70a08231") &&
+      bytecode.contains("dd62ed3e") &&
+      bytecode.contains("a9059cbb") &&
+      bytecode.contains("095ea7b3") &&
+      bytecode.contains("23b872dd")
+  }
+
 }
