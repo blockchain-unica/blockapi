cbe3e979b4c81865815e2a7692dd3a28e32f6415
Alessio Manai <alessio@manai.tk>
2018-04-22 17:18:21 +0200

    get token name and symbol methods added

--- src/main/scala/tcs/blockchain/ethereum/EthereumContract.scala
+++ src/main/scala/tcs/blockchain/ethereum/EthereumContract.scala
@@ -2,6 +2,10 @@ package tcs.blockchain.ethereum
 
 import java.util.Date
 
+import org.apache.commons.codec.DecoderException
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.lang3.StringUtils
+
 
 case class EthereumContract(
                              val name: String,
@@ -29,7 +33,7 @@ case class EthereumContract(
   }
 
   /**
-    * This method check if the contract is ERC20 compliant analyzing the bytecode
+    * This method checks if the contract is ERC20 compliant analyzing the bytecode
     * source: https://ethereum.stackexchange.com/questions/38381/how-can-i-identify-that-transaction-is-erc20-token-creation-contract?answertab=oldest#tab-top
     *
     * @return true if the contract is ERC20 compliant, false otherwise
@@ -42,6 +46,64 @@ case class EthereumContract(
       bytecode.contains("a9059cbb") &&
       bytecode.contains("095ea7b3") &&
       bytecode.contains("23b872dd")
+  }
+
+  /**
+    * This method checks if there is a token name in bytecode, if not returns "Unknown"
+    *
+    * @return token's name
+    */
+
+  def getTokenName(): String = {
+
+    try {
+
+      val name = new String(
+        Hex.decodeHex(
+          StringUtils.substringBetween(bytecode, "81526020017f", "00").toCharArray
+        ), "UTF-8")
+
+      if (name == "address,bytes)"){
+        "Unknown"
+      } else {
+        name
+      }
+
+    } catch {
+      case np : NullPointerException => "Unknown"
+      case de : DecoderException => "Unknown"
+    }
+
+  }
+
+  /**
+    * TThis method checks if there is a token symbol in bytecode, if not returns "Unknown"
+    *
+    * @return token's symbol
+    */
+
+  def getTokenSymbol(): String = {
+
+    try {
+
+      val temp = StringUtils.substringAfter(bytecode, "81526020017f")
+
+      val symbol = new String(
+        Hex.decodeHex(
+          StringUtils.substringBetween(temp, "81526020017f", "00").toCharArray
+        ), "UTF-8")
+
+      if (name == "address,bytes)"){
+        "Unknown"
+      } else {
+        symbol
+      }
+
+    } catch {
+      case np : NullPointerException => "Unknown"
+      case de : DecoderException => "Unknown"
+    }
+
   }
 
 }
--- src/main/scala/tcs/examples/ethereum/mongo/EthereumTokens.scala
+++ src/main/scala/tcs/examples/ethereum/mongo/EthereumTokens.scala
@@ -13,18 +13,23 @@ object EthereumTokens {
     val tokens = new Collection("EthereumTokens", mongo)
 
     // Iterating each block
-    blockchain.start(4880000).end(4881012).foreach(block => {
+    blockchain.start(5058444).end(5061444).foreach(block => {
       if(block.height%100 == 0){
         println("Current Block " + block.height)
       }
       block.txs.foreach(tx => {
           if (tx.hasContract && tx.contract.isERC20Compliant){
+
             tokens.append(
               List(
                 ("contractAddress", tx.contract.address),
                 ("txhash",tx.contract.hashOriginatingTx),
-                ("date",tx.date) /*,
-                ("balance",EthereumTransaction.getContractBalance(tx.contract.address)),
+                ("date",tx.date),
+                ("bytecode", tx.contract.bytecode),
+                ("tokenName", tx.contract.getTokenName()),
+                ("tokenSymbol", tx.contract.getTokenSymbol())
+//                ("tokenDivisibility", tx.contract.getTokenDivisibility())
+                /*("balance",EthereumTransaction.getContractBalance(tx.contract.address)),
                 ("transactionCountFrom",EthereumTransaction.getContractTransactionCount(tx.contract.address))*/
               )
             )
