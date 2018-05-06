ed8c8ad39e072da9c68bffe998aa4785aef46cc6
pow39 <39010952+pow39@users.noreply.github.com>
2018-05-06 08:33:18 +0200

    small refactoring, decoupled settings

---
+++ src/main/scala/tcs/examples/ethereum/mongo/crossValidation/CrossValidationSettings.scala
@@ -0,0 +1,8 @@
+package tcs.examples.ethereum.mongo.crossValidation
+
+object CrossValidationSettings {
+  val EthereumURL = "http://localhost:8545"
+  val DatabaseName = "myDatabase"
+  val CollectionName = "ethCrossValidation"
+}
+
