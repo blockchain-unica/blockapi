1600eb882d9edcc1b5b083163d9bb5cd4a79a1aa
daniele26041993@yahoo.it <ironmaiden93>
2018-06-29 20:20:40 +0200

    Block hash works

--- src/main/scala/it/unica/blockchain/analyses/litecoin/mongo/CrossValidationLitecoin.scala
+++ src/main/scala/it/unica/blockchain/analyses/litecoin/mongo/CrossValidationLitecoin.scala
@@ -4,21 +4,24 @@ import it.unica.blockchain.blockchains.BlockchainLib
 import it.unica.blockchain.blockchains.litecoin.{LitecoinSettings, MainNet}
 import it.unica.blockchain.db.DatabaseSettings
 import it.unica.blockchain.mongo.Collection
+import play.api.libs.json.Json
+import scalaj.http.Http
 
 object CrossValidationLitecoin {
-  def main(args: Array[String]): Unit= {
-    val initialBlock:Int = 1447684
-    val finalBlock:Int = 1447688
+  def main(args: Array[String]): Unit = {
+    val initialBlock: Int = 1447684
+    val finalBlock: Int = 1447688
     val dbMongo = new DatabaseSettings("litecoinDB")
 
     getDataFromTool(initialBlock, finalBlock, dbMongo)
+    getDataFromBlockCypher(initialBlock, finalBlock, dbMongo)
 
   }
 
-  def getDataFromTool(initialBlock:Int, finalBlock:Int, dbMongo:DatabaseSettings): Unit= {
+  def getDataFromTool(initialBlock: Int, finalBlock: Int, dbMongo: DatabaseSettings): Unit = {
     val blockchain = BlockchainLib.getLitecoinBlockchain(new LitecoinSettings("user", "password", "9333", MainNet))
     val toolBlockchain = new Collection("litecoinToolValidation", dbMongo)
-    var blockId:Int = initialBlock
+    var blockId: Int = initialBlock
 
     try {
       blockchain.start(initialBlock).end(finalBlock).foreach(block => {
@@ -26,7 +29,7 @@ object CrossValidationLitecoin {
         println("Current block ID: " + blockId)
 
         block.txs.foreach(tx => {
-          val list = List (
+          val list = List(
             ("hash", tx.hash),
             ("date", tx.date),
             ("inputCount", tx.inputs.length),
@@ -41,25 +44,28 @@ object CrossValidationLitecoin {
 
       toolBlockchain.close
     } catch {
-      case e:Exception => {
+      case e: Exception => {
         toolBlockchain.close
         println("Error while proessing block " + blockId)
         e.printStackTrace()
-        getDataFromTool(blockId+1, finalBlock, dbMongo)
+        getDataFromTool(blockId + 1, finalBlock, dbMongo)
       }
     }
   }
 
-  /*def getDataFromBlockCypher(initialBlock:Int, finalBlock:Int, dbMongo:DatabaseSettings): Unit = {
+  def getDataFromBlockCypher(initialBlock: Int, finalBlock: Int, dbMongo: DatabaseSettings): Unit = {
     val explorerBlockchain = new Collection("litecoinExplorerValidation", dbMongo)
 
-    for(blockId <- initialBlock to finalBlock) {
-      val jsonString =
+    for (blockId <- initialBlock to finalBlock) {
+      val jsonString = Http("https://chain.so/api/v2/get_blockhash/LTC/" + blockId).timeout(1000000000, 1000000000).asString.body
+      val jsonObject = Json.parse(jsonString)
+      val blockHash = (jsonObject \ "data" \\ "blockhash").toString()
+      val finalBlockHash = blockHash.substring(6,blockHash.length-2)
+
+      println(finalBlockHash)
+
     }
-
-  }*/
-
-
 
 
   }
+}
