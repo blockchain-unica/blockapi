5e250a6120e8a34d2f683338fea4a1e2ee8d145e
MrBenzene <45393738+MrBenzene@users.noreply.github.com>
2020-04-08 17:03:38 +0200

    Added two functions to get transaction rates in a time frame

--- src/main/scala/it/unica/blockchain/externaldata/rates/BitcoinRates.scala
+++ src/main/scala/it/unica/blockchain/externaldata/rates/BitcoinRates.scala
@@ -3,10 +3,14 @@ package it.unica.blockchain.externaldata.rates
 import java.io.{BufferedReader, IOException, InputStreamReader}
 import java.net.{HttpURLConnection, URL}
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.util.{Calendar, Date}
 
 import com.codesnippets4all.json.parsers.JsonParserFactory
 import org.apache.commons.lang3.time.DateUtils
+import play.api.libs.json.Json
+import scalaj.http.Http
+
+import scala.collection.mutable
 
 /**
   * Created by Livio on 13/06/2017.
@@ -16,6 +20,9 @@ object BitcoinRates {
   var price: Double = 0
   var prices = scala.collection.mutable.Map("USD"->0.0, "EUR"->0.0, "GBP"->0.0, "JPY"->0.0, "CNY"->0.0)
   val managedCurrencies: List[String] = List("USD", "EUR", "GBP", "JPY", "CNY")
+
+  var BTCtoUSD_JSON = new String
+  var USDToOther_JSON = new String
 
   def getRate(date: Date): Double = {
 
@@ -120,5 +127,70 @@ object BitcoinRates {
       }
     }
     return prices(currency)
+  }
+
+  def getRate_Mod(date: Date): mutable.Map[String, Double] = {
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    var date_form = dateFormat.format(date)
+    var USDRate, EURRate, GBPRate, JPYRate, CNYRate : Double = 0
+
+    //Getting the equivalent Calendar of the Date
+    val cal = Calendar.getInstance()
+    cal.setTime(date)
+
+    // Coindesk has no rates before this timestamp
+    if (date.before(new Date(1279411200000l))) return prices
+
+    val BTCtoUSD = Json.parse(BTCtoUSD_JSON)
+    USDRate = (BTCtoUSD \ "bpi" \ date_form).get.as[Double]
+
+    //On Saturday and Sunday Banks close. For that, there is no exchange data for those days
+    if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY){
+      cal.add(Calendar.DATE, -1)
+      date_form = dateFormat.format(cal.getTime)
+    }
+    if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
+      cal.add(Calendar.DATE, -2)
+      date_form = dateFormat.format(cal.getTime)
+    }
+
+    val USDToOther = Json.parse(USDToOther_JSON)
+    EURRate = (USDToOther \ "rates" \ date_form \ "EUR").get.as[Double] * USDRate
+    GBPRate = (USDToOther \ "rates" \ date_form \ "GBP").get.as[Double] * USDRate
+    JPYRate = (USDToOther \ "rates" \ date_form \ "JPY").get.as[Double] * USDRate
+    CNYRate = (USDToOther \ "rates" \ date_form \ "CNY").get.as[Double] * USDRate
+
+    prices += ("USD" -> USDRate)
+    prices += ("EUR" -> EURRate)
+    prices += ("GBP" -> GBPRate)
+    prices += ("JPY" -> JPYRate)
+    prices += ("CNY" -> CNYRate)
+
+    return prices
+  }
+
+  def setRate(start: Date, end: Date) = {
+    val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+    var start_string = dateFormat.format(start)
+    val end_string = dateFormat.format(end)
+
+    //Getting the equivalent Calendar of the Date
+    val cal = Calendar.getInstance()
+    cal.setTime(start)
+    //On Saturday and Sunday Banks close. For that, there is no exchange data for those days
+    if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY){
+      cal.add(Calendar.DATE, -1)
+      start_string = dateFormat.format(cal.getTime)
+    }
+    if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
+      cal.add(Calendar.DATE, -2)
+      start_string = dateFormat.format(cal.getTime)
+    }
+
+    // Coindesk has no rates before this timestamp
+    if (start.before(new Date(1279411200000l))) start_string = "2010-07-17"
+    BTCtoUSD_JSON = Http("https://api.coindesk.com/v1/bpi/historical/close.json?start="+ start_string +"&end="+ end_string).asString.body
+    if (start.before(new Date(1279411200000l))) start_string = "2010-07-16"
+    USDToOther_JSON = Http("https://api.exchangeratesapi.io/history?base=USD&symbols=EUR,GBP,JPY,CNY&start_at="+ start_string +"&end_at="+ end_string).asString.body
   }
 }
