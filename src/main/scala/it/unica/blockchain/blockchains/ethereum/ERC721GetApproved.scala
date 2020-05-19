2eecbf3bde52b3a4eb0d89baf2948802cd0415cc
MrBenzene <45393738+MrBenzene@users.noreply.github.com>
2020-05-19 21:06:21 +0200

    Added ERC721GetApproved class

---
+++ src/main/scala/it/unica/blockchain/blockchains/ethereum/ERC721GetApproved.scala
@@ -0,0 +1,57 @@
+package it.unica.blockchain.blockchains.ethereum
+
+import java.util.Date
+
+import org.web3j.abi.TypeDecoder
+import org.web3j.abi.datatypes.Address
+import org.web3j.abi.datatypes.generated.Uint256
+import org.web3j.protocol.core.Request
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt
+
+class ERC721GetApproved (
+                          hash: String,
+                          date: Date,
+
+                          nonce: BigInt,
+                          blockHash: String,
+                          blockHeight: BigInt,
+                          transactionIndex: BigInt,
+                          from: EthereumAddress,
+                          to: EthereumAddress,
+                          value: BigInt,
+                          gasPrice: BigInt,
+                          gas: BigInt,
+                          input: String,
+                          addressCreated: EthereumAddress,
+                          publicKey: String,
+                          raw: String,
+                          r: String,
+                          s: String,
+                          v: Int,
+
+                          contract: EthereumContract,
+                          requestOpt: Option[Request[_, EthGetTransactionReceipt]],
+
+                          val method: String,
+                          val tokenId: Uint256
+                        ) extends ERC20Transaction(hash, date, nonce, blockHash, blockHeight, transactionIndex, from, to, value, gasPrice, gas, input, addressCreated, publicKey, raw, r, s, v, contract, requestOpt) {
+
+}
+
+object ERC721GetApproved {
+
+  def getInputData(inputData: String): (String, Uint256) = {
+    val firstArg = 10
+
+    val method: String = "getApproved(uint256 _tokenId)"
+
+    val tokenId: String = inputData.substring(firstArg)
+
+    val refMethod = classOf[TypeDecoder].getDeclaredMethod("decode", classOf[String], classOf[Class[_]])
+    refMethod.setAccessible(true)
+
+    val id = refMethod.invoke(null, tokenId, classOf[Uint256]).asInstanceOf[Uint256]
+
+    return (method, id)
+  }
+}
