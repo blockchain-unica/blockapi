f2f3bc99d549a116e4868b471a39a707e841770c
MrBenzene <45393738+MrBenzene@users.noreply.github.com>
2020-05-28 21:08:02 +0200

    Modified TargetList to be able to work with Ethereum address

--- src/main/scala/it/unica/blockchain/blockchains/ethereum/tokenUtils/TargetList.scala
+++ src/main/scala/it/unica/blockchain/blockchains/ethereum/tokenUtils/TargetList.scala
@@ -1,6 +1,13 @@
 package it.unica.blockchain.blockchains.ethereum.tokenUtils
 
+import it.unica.blockchain.blockchains.ethereum.EthereumAddress
+
 import scala.collection.mutable.ListBuffer
+
+/** This object mantains a list of target addresses during the analisys.
+  * If you want to check informations about only one or some addresses
+  * into the blockchain you can add them into this object.
+  */
 
 object TargetList {
   private val listTarget: ListBuffer[String] = ListBuffer()
@@ -9,11 +16,11 @@ object TargetList {
     listTarget
   }
 
-  def add(address: String){
-    listTarget += address
+  def add(address: EthereumAddress){
+    listTarget += address.address
   }
 
-  def contains(address : String): Boolean ={
-    listTarget.contains(address)
+  def contains(address : EthereumAddress): Boolean ={
+    listTarget.contains(address.address)
   }
 }
